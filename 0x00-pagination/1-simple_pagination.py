41bb65e88e686a9e9bd20c3e154e605657a9d4fd
youssef nagib <youssefnagib14@yahoo.com>
2024-10-27 05:16:26 +0300

    pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -47,11 +47,13 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        get the page
+        """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        start_index, end_index = index_range(page, page_size)
+        start, end = index_range(page, page_size)
         data = self.dataset()
-
-        if start_index > len(data):
+        if start > len(data):
             return []
-        return data[start_index:end_index]
+        return data[start:end]
