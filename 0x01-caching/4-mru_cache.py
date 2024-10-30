06b9cb54734269d0a7c56bd31002f284e35c8100
youssef nagib <youssefnagib14@yahoo.com>
2024-10-30 13:31:15 +0300

    caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+MRU caching module'''
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    '''
+    BaseCache inherits from BaseCaching and implements caching functionality
+    '''
+    def __init__(self):
+        """Initializes the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        '''
+        Add an item in the cache
+        '''
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                last_recently_use, _ = self.cache_data.popitem(last=True)
+                print("DISCARD:", last_recently_use)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        '''
+        Get an item by key
+        '''
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=True)
+        return self.cache_data.get(key, None)
