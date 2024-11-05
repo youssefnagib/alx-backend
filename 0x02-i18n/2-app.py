1a414c4d88ca348e1a0a29d1d38bef1179764f63
youssef nagib <youssefnagib14@yahoo.com>
2024-11-05 22:37:12 +0200

    flask

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''Flask babel'''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Configuration class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    '''return locale'''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index():
+    '''return hello world html file'''
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    '''run'''
+    app.run(debug=True)
