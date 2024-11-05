a8704eddb0607130da4664bc0154adef7a1f4f36
youssef nagib <youssefnagib14@yahoo.com>
2024-11-06 00:27:37 +0200

    flask

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+'''Flask babel'''
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+import pytz
+
+
+class Config:
+    """
+    Configuration class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    '''return user'''
+    user_id = request.args.get('login_as')
+    if user_id and int(user_id) in users:
+        return users[int(user_id)]
+    return None
+
+
+@app.before_request
+def before_request():
+    """Set the user before every request."""
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale() -> str:
+    '''return locale'''
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone() -> str:
+    '''returns the timezone'''
+    timezone = request.args.get('timezone')
+    if timezone:
+        try:
+            pytz.timezone(timezone)
+            return timezone
+        except pytz.UnknownTimeZoneError:
+            pass
+    if g.user and g.user.get('timezone'):
+        try:
+            pytz.timezone(g.user['timezone'])
+            return g.user['timezone']
+        except pytz.UnknownTimeZoneError:
+            pass
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+@app.route('/')
+def index() -> str:
+    '''return hello world html file'''
+    return render_template('7-index.html')
+
+
+if __name__ == '__main__':
+    '''run'''
+    app.run(debug=True)
